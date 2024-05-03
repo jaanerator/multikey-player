91b84026ebe31e357f2e20190336f27fe55c95f0
jaanerator <smin3034@naver.com>
2024-05-03 22:51:08 +0900

    feat: Add Tkinter GUI

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
 import keyboard
 import pyautogui as pg
+from tkinter import Tk
+from tkinter.ttk import Frame as frame
 
 
-if __name__ == "__main__":
+class FrameController(frame):
+    def __init__(self, ui_configs):
+        self.ui_configs = ui_configs
+        self._set_widget()
+        self._init_interface()
+    
+    def mainloop(self):
+        self.window.mainloop()
+    
+    def _set_widget(self):
+        self.window = Tk()
+
+    def _init_interface(self):
+        super().__init__()
+        master_title = self.ui_configs.get("master_title", "None Name")
+        self.master.title(master_title)
+
+        # Set window size
+        w, h = self.ui_configs.get("window_size", (850, 600))
+        sw = self.master.winfo_screenwidth()
+        sh = self.master.winfo_screenheight()
+        x = (sw - w) / 2
+        y = (sh - h) / 2
+        self.master.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+
+def main():
     print("ESC키 입력 시 프로그램이 종료됩니다.")
     print("q키 입력 시 ctrl + alt + shift + s 매크로가 실행됩니다.")
     i = 0
@@ -16,3 +44,9 @@ if __name__ == "__main__":
             pg.hotkey("ctrl", "alt", "shift", "s")
         else:
             pass
+
+
+if __name__ == "__main__":
+    # Execution
+    fc = FrameController({})
+    fc.mainloop()
